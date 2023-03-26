feafc1af8e8dbbc250c56fafdb5131515966c6e8
Nirmalya838 <nirmalya.consultant>
2023-03-26 15:40:57 +0530

    Get element by class name task

---
+++ newdom.js
@@ -0,0 +1,7 @@
+let item = document.getElementsByClassName("list-group-item")
+console.log(item)
+item[2].style.backgroundColor ="green"
+for(let i=0;i<item.length;i++)
+{
+    item[i].style.color ="#2340fa"
+}
