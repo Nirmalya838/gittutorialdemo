6f307c401fdd8cb225e4eb0099def2bc3c50be36
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-04-06 15:49:21 +0530

    Delete date from localstorage

--- dom.js
+++ dom.js
@@ -1,65 +1,55 @@
-// Select the filter input and the list of items
-const filter = document.querySelector('#filter');
-const items = document.querySelector('#items');
+function savedata(event)
+{
+    event.preventDefault();
+    let firstname = event.target.fname.value;
+    let lastname = event.target.lname.value;
+    let address = event.target.add.value;
+    let city = event.target.city.value;
+    let state = event.target.state.value;
+    let pincode = event.target.pin.value;
+    let phone = event.target.ph.value;
+    let date = event.target.date.value;
 
-// Add event listener to filter input
-filter.addEventListener('keyup', filterItems);
+    localStorage.setItem("Firstname", firstname);
+    localStorage.setItem("Lastname", lastname);
+    localStorage.setItem("Address", address);
+    localStorage.setItem("City", city);
+    localStorage.setItem("State", state);
+    localStorage.setItem("Pincode", pincode);
+    localStorage.setItem("Phone", phone);
+    localStorage.setItem("Date", date);
 
-// Function to filter the items based on search string
-function filterItems(e) {
-    // Get search string and convert to lowercase
-    const searchText = e.target.value.toLowerCase();
-    // Get all items
-    const items = itemList.getElementsByTagName('li');
-    // Convert to array and loop through each item
-    Array.from(items).forEach(function(item) {
-      const itemName = item.firstChild.textContent;
-      const itemDesc = item.lastChild.textContent;
-      // Check if search string is found in item name or description
-      if(itemName.toLowerCase().indexOf(searchText) !== -1 || itemDesc.toLowerCase().indexOf(searchText) !== -1) {
-        item.style.display = 'block';
-      } else {
-        item.style.display = 'none';
-      }
-    });
-  }
-  
+    let cli ={
+        firstname,
+        lastname,
+        address,
+        city,
+        state,
+        pincode,
+        phone,
+        date
+    }
+    let res=JSON.stringify(cli)
+    localStorage.setItem(firstname, res)
+    console.log(localStorage)
+    showuser(cli)
 
-// Add event listener to addForm
-const addForm = document.querySelector('#addForm');
-addForm.addEventListener('submit', addItem);
+    function showuser(cli)
+    {   
+        let parentElem= document.getElementById("listOfitems");
+        let childElem = document.createElement("li");
+        childElem.textContent = cli.firstname + "-" + cli.city + "-" + cli.date
+        parentElem.appendChild(childElem);
+        
+        let deletebutton = document.createElement("input");
+        deletebutton.type = "button";
+        deletebutton.value = "Delete";
+        deletebutton.onclick = () => {
+            localStorage.removeItem(cli.email);
+            parentElem.removeChild(childElem);
 
-// Function to add an item to the list
-function addItem(e) {
-  // Prevent the form from submitting
-  e.preventDefault();
-
-  // Get the name and description of the new item from the input fields
-  const itemName = addForm.querySelector('#item').value;
-  const itemDescription = addForm.querySelector('#description').value;
-
-  // Create a new list item with the name and description
-  const newItem = document.createElement('li');
-  newItem.className = 'list-group-item';
-  newItem.innerHTML = `${itemName} - <span class="description">${itemDescription}</span> <button class="btn btn-danger btn-sm float-right delete">X</button>`;
-
-  // Add the new item to the list
-  items.appendChild(newItem);
-
-  // Clear the input fields
-  addForm.querySelector('#item').value = '';
-  addForm.querySelector('#description').value = '';
-}
-
-// Add event listener to items list
-items.addEventListener('click', deleteItem);
-
-// Function to delete an item from the list
-function deleteItem(e) {
-  // Check if the delete button was clicked
-  if (e.target.classList.contains('delete')) {
-    // Remove the item from the list
-    const listItem = e.target.parentElement;
-    items.removeChild(listItem);
-  }
+        }
+        childElem.appendChild(deletebutton)
+        parentElem.appendChild(childElem)
+    }
 }
